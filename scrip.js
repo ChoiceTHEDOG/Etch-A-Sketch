f072acba127f29cef6b29c95deddf5aedb4777d5
valentin <valen.manestar@gmail.com>
2023-01-16 20:17:13 -0300

    añadido el efecto cuando el cursor pasa sobre las grids

--- scrip.js
+++ scrip.js
@@ -1,17 +1,29 @@
 const mainContainer = document.querySelector("div");
 mainContainer.textContent = " ";
 
+// creation of the grid container
 const gridDivContainer = document.createElement("div");
 gridDivContainer.setAttribute("id", "gridContainerDiv");
 mainContainer.appendChild(gridDivContainer)
+
+function getGridSize(){
+    prompt("Enter a number between 1-100 to set the grid size")
+}
 
 function createGridItem(size){
     gridDivContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     for(let i = 0; i < size * size; i++){
         const item = document.createElement("div");
         item.classList.add("cell");
+        item.addEventListener("mouseenter", () => {
+            item.classList.add("hoverOn");
+        });
         gridDivContainer.appendChild(item);
     }
 }
 
 createGridItem(16);
+
+// function hoverOn(){
+//     item.classList.add("hoverOn");
+// }
